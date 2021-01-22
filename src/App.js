8da39eb748b43a2d6779f9aeb216eb49ec50ed2f
chinedunsidinanya <34515761+chinedunsidinanya@users.noreply.github.com>
2021-01-21 22:11:24 -0600

    testing more on the build phase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello react application on docker
+          Hello react application on docker....
         </p>
         <a
           className="App-link"
